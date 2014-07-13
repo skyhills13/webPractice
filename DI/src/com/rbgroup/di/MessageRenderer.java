2d4e221c70bbb6d363afb5453e79bd7a0c9c36e7
skyhills13 <skyhills13@gmail.com>
2014-07-13 15:09:37 +0900

    [DI 1단계] setMessageProvider이용, 유연성 증가 -그러나, 중복 코드가 여전히 존재

--- DI/src/com/rbgroup/di/MessageRenderer.java
+++ DI/src/com/rbgroup/di/MessageRenderer.java
@@ -1,18 +1,22 @@
 package com.rbgroup.di;
 
 public class MessageRenderer {
+	private MessageProvider messageProvider;
+	
+	public void setMessageProvider(MessageProvider messageProvider) {
+		this.messageProvider = messageProvider;
+	}
+	
 	public void render(){
-		MessageProvider mp = new HelloWorldMessageProvider();
-		/*HiWorld를 찍고 싶을 때는 
-		MessageProvider mp = new HiWorldMessageProvider();
-		message renderer클래스가 하위 구현체에 해당하는 HelloWorldMessageProvider 클래스와 tight한 coupling이 발생한 상태. 
-		이 coupling을 끊어서 클래스간 의존 관계를 줄이는 것이 DI 핵심 내용 
-		*/
-		System.out.println(mp.getMessage());
+		System.out.println(messageProvider.getMessage());
 	}
 	
 	public static void main(String[] args){
 		MessageRenderer renderer = new MessageRenderer();
+		renderer.setMessageProvider(new HelloWorldMessageProvider());
+		renderer.render();	
+
+		renderer.setMessageProvider(new HiWorldMessageProvider());
 		renderer.render();	
 	}
 }
