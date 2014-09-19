b0dff256a1d2aa5300cdab6e06c51f67c226ee45
skyhills13 <skyhills13@gmail.com>
2014-09-19 16:38:01 +0900

    DaoFactory에서 새로운 Dao를 추가할시 중복되는 코드를 메소드로 추

--- TobySpring/src/main/java/tobyspring/dao/users/DaoFactory.java
+++ TobySpring/src/main/java/tobyspring/dao/users/DaoFactory.java
@@ -2,9 +2,23 @@ package tobyspring.dao.users;
 
 public class DaoFactory {
 	
-	public UserDao userDao() {
+	public ConnectionMaker connectionMaker(){
 		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDao userDao = new UserDao(connectionMaker);
+		return connectionMaker;
+	}
+	
+	public UserDao userDao() {
+		UserDao userDao = new UserDao(connectionMaker());
+		return userDao;
+	}
+
+	public UserDao accountDao() {
+		UserDao userDao = new UserDao(connectionMaker());
+		return userDao;
+	}
+	
+	public UserDao messageDao() {
+		UserDao userDao = new UserDao(connectionMaker());
 		return userDao;
 	}
 }
