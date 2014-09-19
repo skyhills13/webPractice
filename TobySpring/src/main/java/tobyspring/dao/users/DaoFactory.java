84727ce09702756a2044caf9200095481caaa75d
skyhills13 <skyhills13@gmail.com>
2014-09-19 16:53:10 +0900

    ApplicationConext 클래스를 활용한, bean등록 및 사용

--- TobySpring/src/main/java/tobyspring/dao/users/DaoFactory.java
+++ TobySpring/src/main/java/tobyspring/dao/users/DaoFactory.java
@@ -1,22 +1,30 @@
 package tobyspring.dao.users;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//스프링이 빈팩토리(빈의 생성과 관계 설정 같은 제어를 담당하는 IoC 오브젝트)를 위한 오브젝트 설정을 담당하는 클래스라고 인식할 수 있도록 추가한 annotation
+@Configuration
 public class DaoFactory {
 	
+	@Bean
 	public ConnectionMaker connectionMaker(){
 		ConnectionMaker connectionMaker = new DConnectionMaker();
 		return connectionMaker;
 	}
 	
+	@Bean
 	public UserDao userDao() {
 		UserDao userDao = new UserDao(connectionMaker());
 		return userDao;
 	}
-
+	
+	@Bean
 	public UserDao accountDao() {
-		UserDao userDao = new UserDao(connectionMaker());
-		return userDao;
+		return new UserDao(new DConnectionMaker());
 	}
 	
+	@Bean
 	public UserDao messageDao() {
 		UserDao userDao = new UserDao(connectionMaker());
 		return userDao;
--- TobySpring/src/test/java/tobyspring/dao/users/UserDaoTest.java
+++ TobySpring/src/test/java/tobyspring/dao/users/UserDaoTest.java
@@ -1,6 +1,9 @@
 package tobyspring.dao.users;
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import tobyspring.dao.users.UserDao;
 import tobyspring.domain.users.User;
 
@@ -8,7 +11,13 @@ import tobyspring.domain.users.User;
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		UserDao dao = new DaoFactory().userDao();
+		//DaoFactory처럼 @Configuration이 붙은 자바 코드를 설정 정보로 사용할때 이용하는 클래스
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class); 
+		/*getBean()메소드는 applicationContext가 관리하는 오브젝트를 요청하는 메소드
+		 * 첫번째 파라미터는 applicationContext에 등록된 빈의 이름 (=> DaoFactory에서 @Bean이라는 annotation을 userDao라는 이름의 메소드에 붙였는데, 이 메소드의 이름이 바로 빈의 이름
+		 * 즉, DaoFactory의 userDao() 메소드를 호출해서 그 결과를 가져오는 것. 
+		 */
+		UserDao dao = applicationContext.getBean("userDao", UserDao.class);
 		User user = new User();
 		user.setId("dooby");
 		user.setName("dobyJung");
