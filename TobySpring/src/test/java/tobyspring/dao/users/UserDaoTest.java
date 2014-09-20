a65dcf7c3ea25ef7c0a0176e69fa17bbffe60953
skyhills13 <skyhills13@gmail.com>
2014-09-20 14:51:33 +0900

    main method에서 test 결과 확인까지 자동화

--- TobySpring/src/test/java/tobyspring/dao/users/UserDaoTest.java
+++ TobySpring/src/test/java/tobyspring/dao/users/UserDaoTest.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import com.sun.org.apache.xpath.internal.operations.Equals;
+
 import tobyspring.domain.users.User;
 
 
@@ -18,17 +20,16 @@ public class UserDaoTest {
 		user.setId("dby");
 		user.setName("dbyJung");
 		user.setPassword("ehqlehql");
-		
 		dao.add(user);
-		
-		System.out.println(user.getId() + " : 등록");
 		
 		User user2 = dao.get(user.getId());
 		
-		System.out.println(user2.getName());
-		
-		System.out.println(user2.getPassword());
-		
-		System.out.println(user2.getId() + " : 조회 성공");
+		if(!user.getName().equals(user2.getName())) {
+			System.out.println("테스트 실패 ( name ) ");
+		} else if (!user.getPassword().equals(user2.getPassword())) {
+			System.out.println("테스트 실패 ( password) ");
+		} else {
+			System.out.println("테스트 성공");
+		}
 	}
 }
