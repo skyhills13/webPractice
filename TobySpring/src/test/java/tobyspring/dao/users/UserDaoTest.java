8cfe4af147f1cc16881a7a1b645117f60e97a150
skyhills13 <skyhills13@gmail.com>
2014-09-20 14:08:18 +0900

    불필요한 주석 삭

--- TobySpring/src/test/java/tobyspring/dao/users/UserDaoTest.java
+++ TobySpring/src/test/java/tobyspring/dao/users/UserDaoTest.java
@@ -11,13 +11,6 @@ import tobyspring.domain.users.User;
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-//		//DaoFactory처럼 @Configuration이 붙은 자바 코드를 설정 정보로 사용할때 이용하는 클래스
-//		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class); 
-//		/*getBean()메소드는 applicationContext가 관리하는 오브젝트를 요청하는 메소드
-//		 * 첫번째 파라미터는 applicationContext에 등록된 빈의 이름 (=> DaoFactory에서 @Bean이라는 annotation을 userDao라는 이름의 메소드에 붙였는데, 이 메소드의 이름이 바로 빈의 이름
-//		 * 즉, DaoFactory의 userDao() 메소드를 호출해서 그 결과를 가져오는 것. 
-//		 */
-		
 		ApplicationContext applicationContext = new GenericXmlApplicationContext("applicationContext.xml");
 		UserDao dao = applicationContext.getBean("userDao", UserDao.class);
 		
