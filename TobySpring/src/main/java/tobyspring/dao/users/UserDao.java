7470e01a76c91e410a1fee1f4d070ce0c290bb8b
skyhills13 <skyhills13@gmail.com>
2014-10-24 23:52:44 +0900

    DeleteAllStatement를 익명 내부 클래스로 변환

--- TobySpring/src/main/java/tobyspring/dao/users/UserDao.java
+++ TobySpring/src/main/java/tobyspring/dao/users/UserDao.java
@@ -98,8 +98,13 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException{
-		StatementStrategy st = new DeleteAllStatement();
-		jdbcContextWithStatementStrategy(st);
+		jdbcContextWithStatementStrategy(new StatementStrategy(){
+			@Override
+			public PreparedStatement makePreparedStatement(Connection conn) throws SQLException {
+				PreparedStatement ps = conn.prepareStatement("delete from users");
+				return ps;
+			}
+		});
 	}
 	
 	
